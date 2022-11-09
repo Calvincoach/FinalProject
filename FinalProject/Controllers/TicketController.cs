b1460d02cf4bdeae73460a85bc451c57fcb4f7d3
Momchil Angelov <momangelov@abv.bg>
2022-11-10 00:36:22 +0200

    feat(added): added initial ticket controller and models + navigation (master-ticketController)

---
+++ FinalProject/Controllers/TicketController.cs
@@ -0,0 +1,26 @@
+﻿using FinalProject.Core.Models.Event;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    [Authorize]
+    public class TicketController : Controller
+    {
+        public async Task<IActionResult> MyTicket()
+        {
+            var model = new TicketsModel();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Buy() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Buy(TicketModel model)
+        { 
+            return RedirectToAction(nameof(MyTicket));
+        }
+    }
+}
